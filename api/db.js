883a0f8c69ef22b94f1d7cb679db18d414d94c89
SUMIT <90766113+Bhaisumit@users.noreply.github.com>
2024-11-15 23:47:05 +0530

    Update db.js

--- api/db.js
+++ api/db.js
@@ -1,8 +1,8 @@
 import mysql from "mysql"
 
 export const db = mysql.createConnection({
-  host:"localhost",
-  user:"root",
+  host:process.env.DB_HOST || "localhost",
+  user:process.env.DB_USER ||"root",
   password: process.env.DB_KEY,
   database:"blog"
 })
